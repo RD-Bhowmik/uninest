4b877e900e75f4666b89c3d1423b5734571c65ac
RD-Bhowmik <ronodeep.bhowmik@g.bracu.ac.bd>
2024-07-06 17:41:39 +0600

    added login register and api

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -5,7 +5,7 @@ import Header from "../Header";
 export default function IndexPage() {
   return (
     <div>
-      <Header />
+      index page here
     </div>
   );
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -8,6 +8,7 @@ export default function RegisterPage() {
   const [password, setPassword] = useState("");
   function registerUser(ev) {
     ev.preventDefault();
+    axios.get("http://localhost:4000/test");
     axios.post('/register', {
       name,
       email,
@@ -20,18 +21,22 @@ export default function RegisterPage() {
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
+
           <input type="text"
             placeholder="name"
             value={name}
-            onChange={ev => setName(ev.target.value)} />    
+            onChange={ev => setName(ev.target.value)} />  
+
           <input type="email"
             placeholder="your@email.com"
             value={email}
             onChange={ev => setEmail(ev.target.value)} />
+
           <input type="password"
             placeholder="password"
             value={password}
             onChange={ev => setPassword(ev.target.value)} />
+
         <button className="primary">Register</button>
         <div className="text-center py-2 text-gray-500 ">Already a member? <Link className="underline text-black" to={"/login"}>Login</Link></div>  
           
