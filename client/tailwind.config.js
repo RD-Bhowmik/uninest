9ca114c1c5b5105f5557b35b389ff52886b44a94
RD-Bhowmik <ronodeep.bhowmik@g.bracu.ac.bd>
2024-09-05 00:48:10 +0600

    client errors fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html", // Include the main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in the src directory
+    "!./node_modules", // Exclude node_modules
   ],
   theme: {
     extend: {
       colors: {
-        primary :"#8900de",
-        sb : "#2b0045"
-      }
+        primary: "#8900de", // Primary color
+        sb: "#2b0045" // Secondary color
+      },
     },
   },
   plugins: [],
