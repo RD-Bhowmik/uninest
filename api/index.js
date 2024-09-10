263a9bba7770a0f5800de5a3ac9abddac060bf12
RD-Bhowmik <ronodeep.bhowmik@g.bracu.ac.bd>
2024-09-10 07:50:17 +0600

    some new fixes

--- api/index.js
+++ api/index.js
@@ -24,116 +24,111 @@ app.use(cors({
     origin: ['http://localhost:5173','http://127.0.0.1/5173']
 }));
 
+mongoose.connect(process.env.MONGO_URL);
 
-// mongoose.connect('mongodb+srv://fahim:amrbracunest665@cluster0.nluvtan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-mongoose.connect(process.env.MONGO_URL)
-
-app.get('/test', (req,res) => {
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', async (req,res) =>{
-    const {name,email,password} = req.body;
-    try { 
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
         const userDoc = await UserModel.create({
             name,
             email,
-            password:bcryptjs.hashSync(password, secret),
-    
+            password: bcryptjs.hashSync(password, secret),
         });
         res.json(userDoc);
-    } catch (e){
+    } catch (e) {
         res.status(422).json(e);
     }
-    
-    
 });
 
-
 app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    const userDoc = await UserModel.findOne({email});
-    if (userDoc){
+    const { email, password } = req.body;
+    const userDoc = await UserModel.findOne({ email });
+    if (userDoc) {
         const passOk = bcryptjs.compareSync(password, userDoc.password);
-        if (passOk){
-            jwt.sign({email:userDoc.email,
-                id : userDoc._id}, jwtSecret, {},(err, token) => {
-                    if (err) throw err;
-                    res.cookie('token',token).json(userDoc);
-                });
-            } else {
-                res.status(422).json('pass not ok');
-            }
+        if (passOk) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json(userDoc);
+            });
         } else {
-            res.json('user not found');
+            res.status(422).json('pass not ok');
         }
-    });
-
+    } else {
+        res.json('user not found');
+    }
+});
 
 app.get('/profile', (req, res) => {
-    const {token} = req.cookies;
-    if (token){
-        jwt.verify(token, jwtSecret, {}, async(err, userData)=> {
-        if (err) throw err; 
-        const {name, email, _id} = await UserModel.findById(userData.id);
-        res.json({name, email, _id});
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            const { name, email, _id } = await UserModel.findById(userData.id);
+            res.json({ name, email, _id });
         });
-    } else{
+    } else {
         res.json(null);
     }
 });
 
-app.post('/logout', (req, res) =>{
+app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
 });
 
-
 app.post('/upload-by-link', async (req, res) => {
-    const {link} = req.body;
-    const  newName = 'photo' + Date.now() + '.jpg' ;
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
     await imageDownloader.image({
-        url : link,
+        url: link,
         dest: __dirname + '/uploads/' + newName,
     });
     res.json(newName);
 });
 
-const photosMiddleware = multer({dest:'uploads/'});
-app.post('/upload',photosMiddleware.array('photos',100) ,(req,res) =>{
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
     const uploadedFiles = [];
-    for (let i=0; i<req.files.length; i++){
-        const {path,originalname} = req.files[i];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
         const parts = originalname.split(".");
-        const ext =  parts[parts.length-1];
-        const newPath = path +"." + ext; 
+        const ext = parts[parts.length - 1];
+        const newPath = path + "." + ext;
         fs.renameSync(path, newPath);
-        uploadedFiles.push(newPath.replace("uploads/",""));
+        uploadedFiles.push(newPath.replace("uploads/", ""));
     }
     res.json(uploadedFiles);
 });
 
-app.post('places', (req, res) => {
-    const {token} = req.cookies;
-    const {title, address, addedPhotos, description,
-        extraInfo, checkIn, checkOut, maxGuests,
-    } = req.body();
-    jwt.verify(token, jwtSecret, {}, async(err, userData)=> {
-        if (err) throw err; 
+app.post('/places', (req, res) => {
+    const { token } = req.cookies;
+    const { title, address, addedPhotos, description, extraInfo, checkIn, checkOut, maxGuests } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
         const placeDoc = await Place.create({
-            owner:userData.id,
-            title, address, photos:addedPhotos, description,
-        extraInfo, checkIn, checkOut,maxGuests,
+            owner: userData.id,
+            title,
+            address,
+            photos: addedPhotos,
+            description,
+            extraInfo,
+            checkIn,
+            checkOut,
+            maxGuests,
         });
         res.json(placeDoc);
-    
     });
 });
 
-app.get('/places',(req,res) =>{
-    const{token} = req.cookies; 
-    jwt.verify(token, jwtSecret, {} , async(err,userData) =>{
-        const{id} = userData;
-        res.json(await Place.find({owner:id}));
+app.get('/places', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        const { id } = userData;
+        res.json(await Place.find({ owner: id }));
     });
 });
 
